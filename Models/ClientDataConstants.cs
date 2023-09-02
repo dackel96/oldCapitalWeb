dfa0089d20fbd550b5091ee99bb7a255408a7b39
Ivan Iliev <ivanilievdackel@gmail.com>
2023-09-02 16:37:16 +0300

    FAQ section creation
    2 hours

--- Models/ClientDataConstants.cs
+++ Models/ClientDataConstants.cs
@@ -6,11 +6,11 @@
         {
             public const int QuestionMaxLength = 255;
 
-            public const int QuestionMinLength = 55;
+            public const int QuestionMinLength = 5;
 
             public const int AnswerMaxLength = 255;
 
-            public const int AnswerMinLength = 55;
+            public const int AnswerMinLength = 5;
         }
     }
 }
