501b9a05160a3c22f3c63674d114f3f44db4f393
Ivan Iliev <ivanilievdackel@gmail.com>
2023-10-14 11:57:36 +0300

    about-us-page

--- Controllers/AboutUsController.cs
+++ Controllers/AboutUsController.cs
@@ -1,12 +1,42 @@
 ﻿namespace oldCapitalWeb.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using oldCapitalWeb.Models;
+    using oldCapitalWeb.Services.Home;
 
+    using static ArticleConstants.Constants.AboutUsPageArticles;
     public class AboutUsController : Controller
     {
+
         public IActionResult Index()
         {
-            return View();
+            var articleList = new List<ArticleViewModel>();
+
+            var headArticle = new ArticleViewModel
+            {
+                Id = FrontEndHeadId,
+                Title = HeadTitle,
+                Text = HeadArticleText,
+            };
+            articleList.Add(headArticle);
+
+            var firstArticle = new ArticleViewModel
+            {
+                Id = FrontEndArticleOneId,
+                Title = SecondArticleTitle,
+                Text = SecondArticleText,
+            };
+            articleList.Add(firstArticle);
+
+            var secondArticle = new ArticleViewModel
+            {
+                Id = FrontEndArticleTwoId,
+                Title = ThirdArticleTitle,
+                Text = ThirdArticleText,
+            };
+            articleList.Add(secondArticle);
+
+            return View(articleList);
         }
     }
 }
